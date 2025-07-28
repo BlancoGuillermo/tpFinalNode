630987488467374fe3a0eeb03daf31eb2189d1b8
BlancoGuillermo <105031998+BlancoGuillermo@users.noreply.github.com>
2025-07-28 02:44:45 -0300

    CRUD

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -14,3 +14,35 @@ export const getProductById = async(req,res)=>{
       res.status(404).json({message: "Product not found"});
    }
 };
+
+export const createProduct = async (newProduct) => {
+  try {
+    const docRef = await addDoc(productsCollection, newProduct);
+    return { id: docRef.id, ...newProduct };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateProduct = async (id, updatedProductData) => {
+  try {
+    const docRef = doc(productsCollection, id);
+    await setDoc(docRef, updatedProductData, { merge: true });
+    return { id, ...updatedProductData };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const deleteProduct = async (id) => {
+  try {
+    const docRef = doc(productsCollection, id);
+    await deleteDoc(docRef);
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+};
+
