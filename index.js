c7c2584adf6601f6dbd76a15243efa395cc21f61
BlancoGuillermo <105031998+BlancoGuillermo@users.noreply.github.com>
2025-07-27 23:48:12 -0300

    Expess

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express();
 app.get('/', (req, res)=>{
     res.json({message: "Bienvenidos a la API REST"});
 });
-git
+
+import productsRouter from './src/routes/products.router.js'
+app.use("/api",productsRouter);
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
