6286f3f11af1f9e18323549eea481a3a63ac746b
BlancoGuillermo <105031998+BlancoGuillermo@users.noreply.github.com>
2025-07-28 00:50:18 -0300

    Get Product By Id

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -2,4 +2,8 @@ import * as Model from "../models/products.model.js"
 
 export const getAllProducts = async() => {
     return await Model.getAllProducts();
-}
+};
+
+export const getProductById = async(id) => {
+    return await Model.getProductById(id);
+};
